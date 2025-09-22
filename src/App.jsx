4895b46933126a672600f3dc1b227edefb683934
Areeb <areebahmad613@gmail.com>
2025-09-22 14:07:59 +0530

    Updated the components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import ProductList from "./pages/ProductList";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
 import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="*" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="wishlist" element={<Wishlist />} />
       </Routes>
     </>
   );
