0b88e04e23c50294f834c9f80b6952b241473de4
Areeb <areebahmad613@gmail.com>
2025-09-23 07:30:10 +0530

    Updated the changes in the wishlist component

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeFromWishlistRequest } from "../features/wishlist/wishlistSlice";
+import { removeFromWishlist } from "../features/wishlist/wishlistSlice";
+import { addToCartRequest } from "../features/cart/cartSlice"; // Make sure this exists
 import "../styles/Wishlist.css";
 
 export default function Wishlist() {
   const wishlistItems = useSelector((state) => state.wishlist.items);
   const dispatch = useDispatch();
+
+  const handleAddToCart = (item) => {
+    dispatch(
+      addToCartRequest({
+        productId: item.id,
+        title: item.title,
+        image: item.image,
+        price: item.price,
+        quantity: 1
+      })
+    );
+  };
 
   return (
     <div className="wishlist-container">
@@ -16,25 +29,35 @@ export default function Wishlist() {
       ) : (
         <div className="wishlist-items">
           {wishlistItems.map((item) => (
-            <div key={item.productId} className="wishlist-item">
+            <div key={item.id} className="wishlist-item">
               {/* Product Image */}
-              <img src={item.image} alt={item.title} className="wishlist-img" />
+              <img
+                src={item.image}
+                alt={item.title}
+                className="wishlist-item-img"
+                onError={(e) => (e.target.src = "/fallback-image.png")}
+              />
 
-              {/* Product Details */}
-              <div className="wishlist-details">
+              {/* Product Title */}
+              <div className="wishlist-item-details">
                 <h3>{item.title}</h3>
-                <p className="wishlist-price">₹ {item.price}</p>
               </div>
 
-              {/* Remove Button */}
-              <button
-                className="remove-btn"
-                onClick={() =>
-                  dispatch(removeFromWishlistRequest(item.productId))
-                }
-              >
-                Remove
-              </button>
+              {/* Buttons */}
+              <div className="wishlist-item-buttons">
+                <button
+                  className="add-to-cart-btn"
+                  onClick={() => handleAddToCart(item)}
+                >
+                  Add to Cart
+                </button>
+                <button
+                  className="remove-btn"
+                  onClick={() => dispatch(removeFromWishlist(item.id))}
+                >
+                  Remove
+                </button>
+              </div>
             </div>
           ))}
         </div>
