ec1cd57f1f5b7dc85fc823db814bab7d66ce6a87
Areeb <areebahmad613@gmail.com>
2025-09-23 07:37:57 +0530

    Updated the changes in the product saga component

--- src/features/products/productSaga.jsx
+++ src/features/products/productSaga.jsx
@@ -1,20 +1,82 @@
-import { call, put, takeLatest } from "redux-saga/effects";
-import api from "../../api/axios";
+// src/features/products/productSaga.jsx
+import { call, put, takeLatest, all } from "redux-saga/effects";
+import axios from "axios";
 import {
   fetchProductsRequest,
   fetchProductsSuccess,
   fetchProductsFailure,
+  fetchProductByIdRequest,
+  fetchProductByIdSuccess,
+  fetchProductByIdFailure,
+  fetchCategoriesRequest,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailure,
+  fetchProductsByCategoryRequest,
+  fetchProductsByCategorySuccess,
+  fetchProductsByCategoryFailure
 } from "./productSlice";
 
-function* fetchProducts() {
+// ---- FETCH ALL PRODUCTS ----
+function* fetchProductsSaga() {
   try {
-    const response = yield call(api.get, "/products");
+    const response = yield call(axios.get, "https://fakestoreapi.com/products");
     yield put(fetchProductsSuccess(response.data));
   } catch (error) {
     yield put(fetchProductsFailure(error.message));
   }
 }
 
-export function* productSaga() {
-  yield takeLatest(fetchProductsRequest.type, fetchProducts);
+// ---- FETCH PRODUCT BY ID ----
+function* fetchProductByIdSaga(action) {
+  try {
+    const response = yield call(
+      axios.get,
+      `https://fakestoreapi.com/products/${action.payload}`
+    );
+    yield put(fetchProductByIdSuccess(response.data));
+  } catch (error) {
+    yield put(fetchProductByIdFailure(error.message));
+  }
+}
+
+// ---- FETCH CATEGORIES ----
+function* fetchCategoriesSaga() {
+  try {
+    const response = yield call(
+      axios.get,
+      "https://fakestoreapi.com/products/categories"
+    );
+    yield put(fetchCategoriesSuccess(response.data));
+  } catch (error) {
+    yield put(fetchCategoriesFailure(error.message));
+  }
+}
+
+// ---- FETCH PRODUCTS BY CATEGORY ----
+function* fetchProductsByCategorySaga(action) {
+  try {
+    let response;
+    if (action.payload === "All") {
+      // Fetch all if category is "All"
+      response = yield call(axios.get, "https://fakestoreapi.com/products");
+    } else {
+      response = yield call(
+        axios.get,
+        `https://fakestoreapi.com/products/category/${action.payload}`
+      );
+    }
+    yield put(fetchProductsByCategorySuccess(response.data));
+  } catch (error) {
+    yield put(fetchProductsByCategoryFailure(error.message));
+  }
+}
+
+export function* productsSaga() {
+  // ← changed from productSaga
+  yield all([
+    takeLatest(fetchProductsRequest.type, fetchProductsSaga),
+    takeLatest(fetchProductByIdRequest.type, fetchProductByIdSaga),
+    takeLatest(fetchCategoriesRequest.type, fetchCategoriesSaga),
+    takeLatest(fetchProductsByCategoryRequest.type, fetchProductsByCategorySaga)
+  ]);
 }
